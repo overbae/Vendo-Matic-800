3af49a8f6857de4039c94fa2d5816ab3b5599141
tristan <tristan.overbay@gmail.com>
2023-04-13 23:04:00 -0500

    module-1-capstone Tristans Branch

--- src/main/java/com/techelevator/Candy.java
+++ src/main/java/com/techelevator/Candy.java
@@ -3,7 +3,7 @@ package com.techelevator;
 import java.math.BigDecimal;
 
 public class Candy extends Item{
-    private String vendingPhrase = "Mumch Munch, Yum!";
+    private String vendingPhrase = "Munch Munch, Yum!";
     public Candy(String name, BigDecimal price, String code, String category, int quantity){
         super(name, price, code, category, quantity);
         this.setVendingPhrase(this.vendingPhrase);
--- src/main/java/com/techelevator/Inventory.java
+++ src/main/java/com/techelevator/Inventory.java
@@ -7,48 +7,112 @@ import java.util.TreeMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Inventory {
-    Map<String, Item> inventory = new TreeMap<>();
 
+    // Method to load inventory data from a CSV file
     public Map<String, Item> inventoryLoader() {
-        File listOfInventory = new File("vendingmachine.csv");
-        try (Scanner inventoryInput = new Scanner(listOfInventory)) {
+        // Create a new hash map to store the inventory data
+        Map<String, Item> inventory = new HashMap<>();
+
+        // Open the inventory file using a try-with-resources block
+        try (Scanner inventoryInput = new Scanner(new File("vendingmachine.csv"))) {
+            // Loop through each line of the file
             while (inventoryInput.hasNextLine()) {
-                String inputLine = inventoryInput.nextLine();
-                String[] itemDescription = inputLine.split("\\|");
-                for (String item : itemDescription) {
-                    if (itemDescription[3].equals("Chip")) {
-                        Chips chip = new Chips(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
-                        inventory.put(itemDescription[0], chip);
-                    } else if (itemDescription[3].equals("Candy")) {
-                        Candy candy = new Candy(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
-                        inventory.put(itemDescription[0], candy);
-                    } else if (itemDescription[3].equals("Drink")) {
-                        Drinks drink = new Drinks(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
-                        inventory.put(itemDescription[0], drink);
-                    } else if (itemDescription[3].equals("Gum")) {
-                        Gum gum = new Gum(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
-                        inventory.put(itemDescription[0], gum);
-                    }
+                // Split the line into individual parts using the pipe character as a delimiter
+                String[] itemDescription = inventoryInput.nextLine().split("\\|");
+
+                // Create a new Item object based on the item type indicated in the file
+                Item item = null;
+                if (itemDescription[3].equals("Chip")) {
+                    item = new Chips(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
+                } else if (itemDescription[3].equals("Candy")) {
+                    item = new Candy(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
+                } else if (itemDescription[3].equals("Drink")) {
+                    item = new Drinks(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
+                } else if (itemDescription[3].equals("Gum")) {
+                    item = new Gum(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
                 }
+
+                // Add the new Item object to the inventory map, using the item code as the key
+                inventory.put(itemDescription[0], item);
             }
         } catch (FileNotFoundException e) {
             System.err.println("The file doesn't exist.");
         }
+
+        // Return the completed inventory map
         return inventory;
     }
 
+    // Method to display the items in the inventory
     public void itemDisplay(Map<String, Item> inventory) {
-        for (Map.Entry<String, Item> item : this.inventory.entrySet()) {
+        // Loop through each item in the inventory map
+        for (Map.Entry<String, Item> item : inventory.entrySet()) {
+            // If the item is out of stock, print a message indicating that it's sold out
             if (item.getValue().getQuantity() == 0) {
                 System.out.println(item.getValue().getCode() + " Item is Sold Out");
-            } else {
+            }
+            // Otherwise, print the item code, name, price, and quantity left
+            else {
                 System.out.println(item.getValue().getCode() + " " + item.getValue().getName() + " " + String.format("%.2f", item.getValue().getPrice()) + ", " + item.getValue().getQuantity() + " left.");
             }
         }
     }
 }
+
+
+
+
+
+
+//public class Inventory {
+//    Map<String, Item> inventory = new TreeMap<>();
+//
+//    public Map<String, Item> inventoryLoader() {
+//        File listOfInventory = new File("vendingmachine.csv");
+//        try (Scanner inventoryInput = new Scanner(listOfInventory)) {
+//            while (inventoryInput.hasNextLine()) {
+//                String inputLine = inventoryInput.nextLine();
+//                String[] itemDescription = inputLine.split("\\|");
+//                for (String item : itemDescription) {
+//                    if (itemDescription[3].equals("Chip")) {
+//                        Chips chip = new Chips(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
+//                        inventory.put(itemDescription[0], chip);
+//                    } else if (itemDescription[3].equals("Candy")) {
+//                        Candy candy = new Candy(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
+//                        inventory.put(itemDescription[0], candy);
+//                    } else if (itemDescription[3].equals("Drink")) {
+//                        Drinks drink = new Drinks(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
+//                        inventory.put(itemDescription[0], drink);
+//                    } else if (itemDescription[3].equals("Gum")) {
+//                        Gum gum = new Gum(itemDescription[1], BigDecimal.valueOf(Double.parseDouble(itemDescription[2])), itemDescription[0], itemDescription[3], 5);
+//                        inventory.put(itemDescription[0], gum);
+//                    }
+//                }
+//            }
+//        } catch (FileNotFoundException e) {
+//            System.err.println("The file doesn't exist.");
+//        }
+//        return inventory;
+//    }
+//
+//    public void itemDisplay(Map<String, Item> inventory) {
+//        for (Map.Entry<String, Item> item : this.inventory.entrySet()) {
+//            if (item.getValue().getQuantity() == 0) {
+//                System.out.println(item.getValue().getCode() + " Item is Sold Out");
+//            } else {
+//                System.out.println(item.getValue().getCode() + " " + item.getValue().getName() + " " + String.format("%.2f", item.getValue().getPrice()) + ", " + item.getValue().getQuantity() + " left.");
+//            }
+//        }
+//    }
+//}
 
 
 
